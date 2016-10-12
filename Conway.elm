95a2e6efc6297a057f2bc8822781e1a9456d517e
Grégory Fouquet <gfouquet@armida.fr>
2016-10-12 22:43:48 +0200

    It works

--- Conway.elm
+++ Conway.elm
@@ -6,11 +6,14 @@ import Html.Attributes as Attr
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import String exposing (toInt)
-import Array exposing (Array, toList, fromList)
+import Array exposing (Array, toList)
 import Debug exposing (..)
 import Css exposing (margin, padding, backgroundColor, height, width, px, rgb)
 import Random exposing (Seed, initialSeed)
-import Time
+import Time exposing (Time, millisecond)
+import Experiment exposing (Experiment, Cell)
+import Coord exposing (..)
+import Maybe exposing (Maybe)
 
 main =
     App.program
@@ -29,11 +32,6 @@ type alias Model =
     , randomSeed : Seed
     }
 
-type alias Experiment = Array (Array Cell)
-type alias Cell = Bool
-type alias Row = Int
-type alias Col = Int
-type alias Coord = (Row, Col)
 type State = Running | Stopped
 
 init : (Model, Cmd Msg)
@@ -41,31 +39,12 @@ init =
     let width = 5
     in
         ({ width = width
-        , experiment = emptyCells width
+        , experiment = Experiment.empty width
         , state = Stopped
         -- random seed should be initialized with current time when I understand how Time.now works
         , randomSeed = initialSeed <| 1234
         }, Cmd.none)
 
-emptyCells : Int -> Experiment
-emptyCells width =
-    Array.initialize width (\r -> Array.initialize width (\c -> False))
-
-randomCells : Seed -> Int -> (Experiment, Seed)
-randomCells initSeed width =
-    let
-        rowSupplier = \s -> Random.step (Random.list width Random.bool) s
-        pairs = List.foldl
-            (\r acc -> case List.head acc of
-                Nothing -> [rowSupplier initSeed]
-                Just (_, seed) -> rowSupplier seed :: acc
-            ) [] [1..width]
-        newSeed = case List.head pairs of
-            Nothing -> initSeed
-            Just (_, seed) -> seed
-    in
-        (log "pairs" pairs |> List.map (\(row,_) -> fromList row) |> fromList
-        , log "new seed" newSeed)
 
 -- UPDATE
 type Msg =
@@ -73,33 +52,34 @@ type Msg =
     | ToggleCell Coord Bool
     | Clear
     | RandomExperiment
+    | Start
+    | Stop
+    | NextBatch Time
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case log "message" msg of
         Resize val ->
             let width = Result.withDefault 0 (toInt val)
-            in ({ model | experiment = emptyCells width, width = width }, Cmd.none)
+            in ({ model | experiment = Experiment.empty width, width = width }, Cmd.none)
         ToggleCell coord cell ->
-            ({ model | experiment = toggle model.experiment coord }, Cmd.none)
-        Clear -> ({ model | experiment = emptyCells model.width }, Cmd.none)
+            ({ model | experiment = Experiment.toggle model.experiment coord }, Cmd.none)
+        Clear -> ({ model | experiment = Experiment.empty model.width }, Cmd.none)
         RandomExperiment -> (randomExperiment (log "prev model" model), Cmd.none)
-
-toggle : Experiment -> Coord -> Experiment
-toggle experiment coord =
-    let (row, col) = coord
-        rcell = Maybe.withDefault Array.empty (Array.get row experiment)
-        cell = Maybe.withDefault False (Array.get col rcell)
-    in Array.set row (Array.set col (not cell) rcell) experiment
+        Start -> ({ model | state = Running }, Cmd.none)
+        Stop -> ({ model | state = Stopped }, Cmd.none)
+        NextBatch time -> ({ model | experiment = Experiment.next model.experiment}, Cmd.none)
 
 randomExperiment : Model -> Model
 randomExperiment model =
-    let (exp, seed) = randomCells model.randomSeed model.width
+    let (exp, seed) = Experiment.random model.randomSeed model.width
     in log "new model" { model | experiment = exp, randomSeed = seed }
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    case model.state of
+        Stopped -> Sub.none
+        Running -> Time.every (100 * millisecond) NextBatch
 
 -- VIEW
 styles =
@@ -112,17 +92,17 @@ view model =
             [ input
                 [ type' "number"
                 , Attr.min "0"
-                , Attr.max "30"
+                , Attr.max "100"
                 , value (toString model.width)
                 , size 5
                 , onInput Resize
                 ] []
-            , input [ type' "button", value "start", disabled (model.state == Running)] []
-            , input [ type' "button", value "stop", disabled (model.state == Stopped)] []
+            , input [ type' "button", value "start", disabled (model.state == Running), onClick Start] []
+            , input [ type' "button", value "stop", disabled (model.state == Stopped), onClick Stop ] []
             , input [ type' "button", value "random", disabled (model.state == Running), onClick RandomExperiment] []
             , input [ type' "button", value "clear", disabled (model.state == Running), onClick Clear] []
             ]
-        , definitionTable model.experiment
+        , if model.state == Stopped then definitionTable model.experiment else div [] []
         , experimentPane model.experiment
         ]
 
@@ -142,8 +122,8 @@ experimentCell row col cell =
     in
         td [ styles
                 [ backgroundColor (style cell)
-                , Css.width (px 5)
-                , Css.height (px 5)
+                , Css.width (px 3)
+                , Css.height (px 3)
                 ]
             ] [text " "]
 
--- Experiment.elm
+++ Experiment.elm
@@ -1,43 +1,80 @@
-module Experiment exposing (Experiment, Cell, empty, random, toggle)
+module Experiment exposing (Experiment, Cell, empty, random, toggle, next)
 
 import Coord exposing (..)
 import Array exposing (Array, fromList, toList)
+import Array as A
 import Random exposing (Seed)
-import List exposing (..)
+import List as L
 import Debug exposing (log)
+import Maybe exposing (withDefault)
 
 type alias Experiment = Array (Array Cell)
 type alias Cell = Bool
 
 empty : Int -> Experiment
 empty width =
-    Array.initialize width (\r -> Array.initialize width (\c -> False))
+    A.initialize width (\r -> A.initialize width (\c -> False))
+
 
 random : Seed -> Int -> (Experiment, Seed)
 random initSeed width =
     let
-        pairs = foldl
-            (\r acc -> case head acc of
-                Nothing -> [rowSupplier width initSeed]
-                Just (_, seed) -> rowSupplier width seed :: acc
+        pairs = L.foldl
+            (\r acc -> case L.head acc of
+                Nothing -> [randomRow width initSeed]
+                Just (_, seed) -> randomRow width seed :: acc
             ) [] [1..width]
 
-        newSeed = case head pairs of
+        newSeed = case L.head pairs of
             Nothing -> initSeed
             Just (_, seed) -> seed
 
     in
-        (log "pairs" pairs |> map (\(row,_) -> fromList row) |> fromList
+        (log "pairs" pairs |> L.map (\(row,_) -> fromList row) |> fromList
         , log "new seed" newSeed)
+
 
 toggle : Experiment -> Coord -> Experiment
 toggle experiment coord =
     let (row, col) = coord
-        rcell = Maybe.withDefault Array.empty (Array.get row experiment)
-        cell = Maybe.withDefault False (Array.get col rcell)
-    in Array.set row (Array.set col (not cell) rcell) experiment
+        rcell = withDefault A.empty (A.get row experiment)
+        cell = withDefault False (A.get col rcell)
+    in A.set row (A.set col (not cell) rcell) experiment
+
+
+next : Experiment -> Experiment
+next experiment =
+    experiment |> A.indexedMap (\rdx row -> row |> A.indexedMap (\cdx cell -> nextCell experiment (rdx,cdx) cell))
+
 
 -- INTERNAL FUNCTIONS
 
-rowSupplier width seed =
+randomRow width seed =
     Random.step (Random.list width Random.bool) seed
+
+
+offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+rule cell liveNeighbors =
+    case cell of
+        False -> if liveNeighbors == 3 then True else False
+        True -> if liveNeighbors == 2 || liveNeighbors == 3 then True else False
+
+neighbor : Array Cell -> Int -> Int -> Cell
+neighbor ar dx off =
+    case A.get (dx + off) ar of
+    Nothing -> False
+    Just n -> n
+
+offsetsToNeighbor : Experiment -> Coord -> Coord -> Cell
+offsetsToNeighbor experiment (rdx, cdx) (x, y) =
+    case A.get (rdx + x) experiment of
+    Nothing -> False
+    Just row -> neighbor row cdx y
+
+nextCell : Experiment -> Coord -> Cell -> Cell
+nextCell experiment coord cell =
+    offsets
+    |> L.map (offsetsToNeighbor experiment coord)
+    |> L.foldl (\c acc -> acc + if c then 1 else 0) 0
+    |> rule cell
